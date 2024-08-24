eb7fe4cd7d4ca97c9278563413c1c822455a4be1
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-08-24 11:05:18 +0500

    delete lib directory

--- lib/bindings/controllers_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:lingogem/controllers/prompt_screen_controller.dart';
-
-class ControllersBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(PromptScreenController());
-  }
-}
